f7ac88ed0a94d5bd71041b317d19eef1abda184b
Alexander Shvets <alex@Alexanders-MacBook-Pro.local>
2013-02-02 14:28:35 -0500

    Initial release

--- spec/resource_accessor_spec.rb
+++ spec/resource_accessor_spec.rb
@@ -5,7 +5,7 @@ require 'resource_accessor/resource_accessor'
 describe ResourceAccessor do
 
   it "should get simple response" do
-    response = subject.get_response :url => "http://www.etvnet.com/catalog/"
+    response = subject.get_response :url => "http://www.lenta.ru"
 
     response.should_not be_nil
   end
